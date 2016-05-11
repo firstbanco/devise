772e4e372619aad45520e2cace9a648f6f15bcc7
Harry Lascelles <harry@bambooloans.com>
2016-05-11 18:39:01 +0100

    Trial 5

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -21,39 +21,39 @@ class Devise::SessionsController < DeviseController
     respond_with resource, location: after_sign_in_path_for(resource)
   end
 
-  # DELETE /resource/sign_out
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-    yield if block_given?
-    respond_to_on_destroy
-  end
+                                              # DELETE /resource/sign_out
+                                              def destroy
+                                                signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+                                                set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
+                                                yield if block_given?
+                                                respond_to_on_destroy
+                                              end
 
-  protected
+                                              protected
 
-  def sign_in_params
-    devise_parameter_sanitizer.sanitize(:sign_in)
-  end
+                                              def sign_in_params
+                                                devise_parameter_sanitizer.sanitize(:sign_in)
+                                              end
 
-                                                                def serialize_options(resource)
-                                                                  methods = resource_class.authentication_keys.dup
-                                                                  methods = methods.keys if methods.is_a?(Hash)
-                                                                  methods << :password if resource.respond_to?(:password)
-                                                                  {methods: methods, only: [:password]}
-                                                                end
+                                                                                                            def serialize_options(resource)
+                                                                                                              methods = resource_class.authentication_keys.dup
+                                                                                                              methods = methods.keys if methods.is_a?(Hash)
+                                                                                                              methods << :password if resource.respond_to?(:password)
+                                                                                                              {methods: methods, only: [:password]}
+                                                                                                            end
 
-  def auth_options
-    {scope: resource_name, recall: "#{controller_path}#new"}
-  end
+                                              def auth_options
+                                                {scope: resource_name, recall: "#{controller_path}#new"}
+                                              end
 
-  def translation_scope
-    'devise.sessions'
-  end
+                                              def translation_scope
+                                                'devise.sessions'
+                                              end
 
-  private
+                                              private
 
-  # Check if there is no signed in user before doing the sign out.
-  #
+                                              # Check if there is no signed in user before doing the sign out.
+                                              #
   # If there is no signed in user, it will set the flash message and redirect
   # to the after_sign_out path.
   def verify_signed_out_user
