e460faeb3958c85e856c42e1d3cf39c69b7d052e
Harry Lascelles <harry@bambooloans.com>
2016-05-11 18:32:48 +0100

    Trial 4

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -10,16 +10,16 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
+                                                                        count: resource.errors.count,
+                                                                        resource: resource.class.model_name.human.downcase)
 
-    html = <<-HTML
-    <div id="error_explanation">
-      <h2>#{sentence}</h2>
-      <ul>#{messages}</ul>
-    </div>
-    HTML
+                                                      html = <<-HTML
+                                                      <div id="error_explanation">
+                                                        <h2>#{sentence}</h2>
+                                                        <ul>#{messages}</ul>
+                                                      </div>
+                                                      HTML
 
-    html.html_safe
-  end
-end
+                                                      html.html_safe
+                                                    end
+                                                  end
